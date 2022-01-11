827c2eba6c0166f6a8c6025186aad656809298c9
Darbaz Ali <darbaz.me@gmail.com>
2022-01-11 09:54:16 +0300

    setup express server

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import { join } from "path"
+
+import mountMiddleware from "./mountMiddleware.js"
+import mountRoutes from "./mountRoutes.js"
+
+const createExpressApp = ({ config, env }) => {
+    const app = express()
+
+    // Configure PUG
+    app.set("views", join(__dirname))
+    app.set("view engine", "pug")
+
+    mountMiddleware(app, env)
+    mountRoutes(app, config)
+
+    return app
+}
+
+export default app
